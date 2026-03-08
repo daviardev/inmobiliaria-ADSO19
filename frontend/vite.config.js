7e7de422d11f8d7970cb27a9c93646a1c6f69b59
daviardev <jerson.silva99@hotmail.com>
2026-03-07 19:48:07 -0500

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,18 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [tailwindcss()],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        login: "./src/pages/auth/login.html",
+        register: "./src/pages/auth/register.html",
+        forgotPassword: "./src/pages/auth/forgot-password.html",
+        reset: "./src/pages/auth/reset.html",
+        panel: "./src/pages/panel/index.html",
+        admin: "./src/pages/admin/index.html",
+      },
+    },
+  },
+});
